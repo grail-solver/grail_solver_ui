f6ad99a1489db0efb5d5c202863421ffa555f119
iamklaus <iamklaus01@gmail.com>
2023-03-05 18:04:19 +0100

    Feat : Wrap context component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,23 +1,31 @@
-import '../Styles/App.css';
+import { useState } from 'react';
 import Editor from './Editor';
 import Formulation from './Formulation';
 import Solution from './Solution';
 import ActionButton from './ActionButton';
+import Notification from './Notification';
+import { ExpandContextProvider } from './ExpandContext';
+
+import '../Styles/App.css';
 
 function App() {
+  const [error_message, setErrorMessage] = useState("")
   return (
-    <div>
+    <div className="app">
         <h2 className="title">Grail Solver</h2>
+        {error_message!=="" && <Notification e={error_message} close={setErrorMessage} />}
         <div className="buttons-container">
-            <ActionButton />
+            <ActionButton setErrorMessage= {setErrorMessage}/>
         </div>
-        <div id="main-bloc" className="block-container">
-            <div className="problem">
-                <Editor/>
-                <Formulation/>
-            </div>
-            <Solution />
-        </div>
+        <ExpandContextProvider>
+          <div id="main-bloc" className="block-container">
+              <div className="problem">
+                  <Editor/>
+                  <Formulation/>
+              </div>
+              <Solution />
+          </div>
+        </ExpandContextProvider>
     </div>
   );
 }
